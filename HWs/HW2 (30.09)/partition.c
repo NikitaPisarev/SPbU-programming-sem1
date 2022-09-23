aba7483ea4c7071820d3e1ad92cf199bbc316321
Johanlib <nikgooder@gmail.com>
2022-09-23 15:07:26 +0300

    Completed partition

---
+++ HWs/HW2 (30.09)/partition.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define arraySize 150
+
+void printArr(int array[])
+{
+    printf("[ ");
+    for (int i = 0; i < arraySize; ++i)
+    {
+        printf("%d ", array[i]);
+
+    }
+    printf("]");
+}
+
+void swap(int *firstNumber, int *secondNumber)
+{
+    int temp = *firstNumber;
+    *firstNumber = *secondNumber;
+    *secondNumber = temp;
+}
+
+void partition(int array[], int index, int rigthElement)
+{
+    int leftElement = index + 1;
+
+    for (int current = leftElement; current < rigthElement; ++current)
+    {
+        if (array[index] > array[current])
+        {
+            swap(&array[current], &array[leftElement]);
+            ++leftElement;
+        }
+    }
+
+    swap(&array[index], &array[leftElement - 1]);
+}
+
+void main()
+{
+    int array[arraySize] = { 0 };
+
+    for (int i = 0; i < arraySize; ++i)
+    {
+        array[i] = rand();
+    }
+
+    printf("The array was like this:\n");
+    printArr(array);
+
+    partition(array, 0, arraySize);
+
+    printf("\nThe array has become like this:\n");
+    printArr(array);
+}
