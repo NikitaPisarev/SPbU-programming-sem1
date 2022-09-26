25f0b82658bea56f6f44f34808441e8066506f5a
Johanlib <nikgooder@gmail.com>
2022-09-27 00:41:26 +0300

    Removed the Else branch

--- HWs/HW1 (21.09)/task2.c
+++ HWs/HW1 (21.09)/task2.c
@@ -13,31 +13,29 @@ int main()
     if (divider == 0)
     {
         printf("Divider can't be zero.\n");
+        return 0;
     }
-    else
+
+    int absDividend = abs(dividend);
+    int absB = abs(divider);
+
+    while (absDividend >= absB)
     {
-        int absDividend = abs(dividend);
-        int absB = abs(divider);
-
-
-        while (absDividend >= absB)
-        {
-            absDividend -= absB;
-            ++quotient;
-        }
-
-        if (dividend < 0 && absDividend != 0)
-        {
-            ++quotient;
-        }
-
-        if ((dividend < 0 && divider > 0) || (dividend > 0 && divider < 0))
-        {
-            quotient = -quotient;
-        }
-
-        printf("The quotient %d by %d is %d.", dividend, divider, quotient);
+        absDividend -= absB;
+        ++quotient;
     }
+
+    if (dividend < 0 && absDividend != 0)
+    {
+        ++quotient;
+    }
+
+    if ((dividend < 0 && divider > 0) || (dividend > 0 && divider < 0))
+    {
+        quotient = -quotient;
+    }
+
+    printf("The quotient %d by %d is %d.", dividend, divider, quotient);
     
     return 0;
 }
