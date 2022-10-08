3bf5ea30445576405af25e2ba2c959d383b4f0f9
Johanlib <nikgooder@gmail.com>
2022-10-09 01:36:07 +0300

    add file extraction and function most

--- HWs/HW4/task2/main.c
+++ HWs/HW4/task2/main.c
@@ -1,79 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "qsort.h"
 
-#define arraySizeBound 999
-
-bool binarySearch(int array[], int firstIndex, int secondIndex, int key)
+void mostCommon(int array[], int length)
 {
-    int middle = (firstIndex + secondIndex) / 2;
-    
-    if (array[middle] == key)
-    {
-        return true;
-    }
-
-    if (firstIndex >= secondIndex)
-    {
-        return false;
-    }   
-
-    if (array[middle] < key)
-    {
-        binarySearch(array, middle + 1, secondIndex, key);
-    }
-    else
-    {
-        binarySearch(array, firstIndex, middle - 1, key);
-    }
-}
-
-void main()
-{
-    int array[arraySizeBound] = { 0 };
-    int arraySize = 0;
-    int scan_res = 0;
-    bool flagInput = true;
-
-    do
-    {
-        printf("Enter size array: ");
-        scan_res = scanf("%d", &arraySize);
-
-        flagInput = true;
-
-        if (!scan_res || arraySize < 1 || arraySize > arraySizeBound)
-        {
-            printf("Incorrect input (The size is positive and no more than %d). Try again!\n", arraySizeBound);
-            scanf_s("%*[^\n]");
-
-            flagInput = false;
-        }
-    } while (!scan_res || !flagInput);
-    
-
-    printf("Enter a array:\n");
-    for (int i = 0; i < arraySize; ++i)
-    {
-        do
-        {
-            scan_res = scanf("%d", &array[i]);
-
-            if (!scan_res)
-            {
-                printf("Incorrect input. Try again!\n");
-                scanf_s("%*[^\n]");
-            }
-        } while (!scan_res);
-    }
-
-    quickSort(array, 0, arraySize - 1);
-
     int mostCommon = array[0];
     int counter = 1;
     int counterMaximum = 1;
 
-    for (int i = 1; i < arraySize; ++i)
+    for (int i = 1; i < length; ++i)
     {
         if (array[i] == array[i - 1])
         {
@@ -93,16 +29,65 @@ void main()
 
     if (counter > counterMaximum)  // If the most common number is at the end, then we never check the last element for the maximum
     {
-        mostCommon = array[arraySize - 2]; 
+        mostCommon = array[length - 2]; 
         counterMaximum = counter;
     }
 
     if (counterMaximum == 1)
     {
-        printf("All elements occur once.");
+        printf("All elements occur once.\n");
     }
     else
     {
-        printf("The most common element is %d, it has been encountered %d times.", mostCommon, counterMaximum);
+        printf("The most common element is %d, it has been encountered %d times.\n", mostCommon, counterMaximum);
     }
+}
+
+int main()
+{
+    FILE *file = fopen("array.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("File opening error :(\n");
+        return 0;
+    }
+
+    int arraySize = 0;
+    int temp = 0;
+
+    while (fscanf(file, "%d", &temp) == 1)
+    {
+        ++arraySize;
+    }
+
+    if (arraySize == 0)
+    {
+        printf("File is empty.\n");
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_SET);
+
+    int *array = calloc(arraySize, sizeof(int));
+
+    if (array == NULL)
+    {
+        printf("Memory allocation error :(\n");
+        return 0;
+    }
+
+    for (int i = 0; i < arraySize; ++i)
+    {
+        fscanf(file, "%d", &array[i]);
+    }
+
+    fclose(file);
+
+    quickSort(array, 0, arraySize - 1);
+    mostCommon(array, arraySize);
+
+    free(array);
+
+    return 0;
 }
