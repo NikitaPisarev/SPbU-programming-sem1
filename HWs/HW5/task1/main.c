d9459b33e13de9da39bc31b4db6a8c3983c8b16c
Johanlib <nikgooder@gmail.com>
2022-10-26 13:56:08 +0300

    Completed task1

--- HWs/HW5/task1/main.c
+++ HWs/HW5/task1/main.c
@@ -1,8 +1,208 @@
 #include <stdio.h>
 #include <stdio.h>
-#include "../stack/stack.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../Stack/stack.h"
 
-void main()
+int operations(Stack *stack, unsigned char sign)
 {
+    int errorCode = 0;
+
+    if (isEmpty(stack))
+    {
+        return -4;
+    }
+
+    int firstValue = 0;
+    if ((errorCode = pop(stack, &firstValue)) != 0)
+    {
+        return errorCode;
+    }
+
+    if (isEmpty(stack))
+    {
+        return -4;
+    }
+
+    int secondValue = 0;
+    if ((errorCode = pop(stack, &secondValue)) != 0)
+    {
+        return errorCode;
+    }
+
+    switch (sign)
+    {
+    case '+':
+        if ((errorCode = push(stack, firstValue + secondValue)) != 0)
+        {
+            return errorCode;
+        }
+
+        break;
+
+    case '-':
+        if ((errorCode = push(stack, secondValue - firstValue)) != 0)
+        {
+            return errorCode;
+        }
+
+        break;
+
+    case '*':
+        if ((errorCode = push(stack, firstValue * secondValue)) != 0)
+        {
+            return errorCode;
+        }
+
+        break;
+
+    case '/':
+        if ((errorCode = push(stack, secondValue / firstValue)) != 0)
+        {
+            return errorCode;
+        }
+
+        break;
+    }
+
+    return 0;
+}
+
+bool testOperations()
+{
+    char testEntry[] = "9 6 - 1 2 + *";
+
+    Stack *stack = createStack();
+    if (stack == NULL)
+    {
+        printf("Stack creation error.\n");
+        return false;
+    }
     
+    for (int i = 0; i < 13; ++i)
+    {
+        unsigned char currentСharacter = testEntry[i];
+
+        if (currentСharacter == ' ')
+        {
+            continue;
+        }
+
+        if (currentСharacter == '+' || currentСharacter == '-' || currentСharacter == '*' || currentСharacter == '/')
+        {
+            if (operations(stack, currentСharacter) != 0)
+            {
+                printf("Error in operations function.\n");
+                freeStack(stack);
+                free(stack);
+                return false;
+            }
+
+            continue;
+        } 
+
+        if (push(stack, (int)currentСharacter - (int)'0') != 0)
+        {
+            printf("Error in push function.\n");
+            freeStack(stack);
+            free(stack);
+            return false;
+        }
+    }
+    
+    int resultTest = 0;
+    if (pop(stack, &resultTest) != 0)
+    {
+        printf("Error in pop function.\n");
+        freeStack(stack);
+        free(stack);
+
+        return false;
+    }
+
+    freeStack(stack);
+    free(stack);
+    
+    return resultTest == 9;
+}
+
+int main()
+{
+    if (!testOperations())
+    {
+        printf("Tests failed!\n");
+        return 0;
+    }
+
+    printf("Enter the expression in reverse Polish notation:\n");
+
+    Stack *stack = createStack();
+    if (stack == NULL)
+    {
+        printf("Stack creation error.\n");
+        return 0;
+    }
+
+    unsigned char currentСharacter = 0;
+    int errorCode = 0;
+
+    while ((currentСharacter = getc(stdin)) != '\n')
+    {
+        if (currentСharacter == ' ')
+        {
+            continue;
+        }
+
+        if (currentСharacter == '+' || currentСharacter == '-' || currentСharacter == '*' || currentСharacter == '/')
+        {
+            if ((errorCode = operations(stack, currentСharacter)) != 0)
+            {
+                if (errorCode == -4)
+                {
+                    printf("This is not reverse Polish notation.\n");
+                }
+                else
+                {
+                    printf("The operations function failed with error %d.\n", errorCode);
+                }
+                freeStack(stack);
+                free(stack);
+                return 0;
+            }
+
+            continue;
+        } 
+
+        if ((int)currentСharacter < 48 || (int)currentСharacter > 57)
+        {
+            printf("Reading error, enter only \"+, -, *, /\" and digits 0-9.\n");
+            freeStack(stack);
+            free(stack);
+            return 0;
+        }
+
+        if ((errorCode = push(stack, (int)currentСharacter - (int)'0')) != 0)
+        {
+            printf("The push function failed with error %d.\n", errorCode);
+            freeStack(stack);
+            free(stack);
+            return 0;
+        }
+    }
+
+    int result = 0;
+    if ((errorCode = pop(stack, &result)) != 0)
+    {
+        printf("The pop function failed with error %d.\n", errorCode);
+        freeStack(stack);
+        free(stack);
+
+        return 0;
+    }
+
+    freeStack(stack);
+    free(stack);
+
+    printf("Ответ: %d", result);
+    return 0;
 }
