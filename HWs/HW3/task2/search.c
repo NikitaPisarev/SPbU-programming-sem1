0d3368bb44d224cdeb2bbfe8b99746ef4f475c94
Johanlib <nikgooder@gmail.com>
2022-11-19 19:10:13 +0300

    Rename the function and fix code style

--- HWs/HW3/task2/search.c
+++ HWs/HW3/task2/search.c
@@ -25,48 +25,47 @@ void arrayRandom(int array[], int length)
     }
 }
 
-bool binarySearch(int array[], int leftIndex, int rightIndex, int key)
+bool binarySearchInSortedArray(int array[], int leftIndex, int rightIndex, int key)
 {
-    int middle = (leftIndex + rightIndex) / 2;
+    if (leftIndex > rightIndex)
+    {
+        return false;
+    }
 
+    int middle = (leftIndex + rightIndex) / 2;
     if (array[middle] == key)
     {
         return true;
     }
 
-    if (leftIndex >= rightIndex)
-    {
-        return false;
-    }
-
     if (array[middle] < key)
     {
-        binarySearch(array, middle + 1, rightIndex, key);
+        binarySearchInSortedArray(array, middle + 1, rightIndex, key);
     }
     else
     {
-        binarySearch(array, leftIndex, middle - 1, key);
+        binarySearchInSortedArray(array, leftIndex, middle - 1, key);
     }
 }
 
 bool testBinarySearch1()
 {
     int array[5] = {1, 2, 3, 4, 5};
-    return binarySearch(array, 0, 4, 4);
+    return binarySearchInSortedArray(array, 0, 4, 4);
 }
 
 bool testBinarySearch2()
 {
     int array[5] = {5, 4, 3, 2, 1};
     quickSort(array, 0, 4);
-    return binarySearch(array, 0, 4, 2);
+    return binarySearchInSortedArray(array, 0, 4, 2);
 }
 
 bool testBinarySearch3()
 {
     int array[6] = {15, 4, -3, 0, 11};
     quickSort(array, 0, 5);
-    return !binarySearch(array, 0, 5, 2);
+    return !binarySearchInSortedArray(array, 0, 5, 2);
 }
 
 int testBinarySearch()
@@ -107,26 +106,26 @@ int main()
 
     int lengthArrayN = 0;
     int lengthArrayK = 0;
-    int scan_resN = 0;
-    int scan_resK = 0;
+    int scanResultN = 0;
+    int scanResultK = 0;
     bool isCorrectInput = true;
 
     do
     {
         printf("Enter the length of the array \"N\" then the array \"K\": ");
-        scan_resN = scanf("%d", &lengthArrayN);
-        scan_resK = scanf("%d", &lengthArrayK);
+        scanResultN = scanf("%d", &lengthArrayN);
+        scanResultK = scanf("%d", &lengthArrayK);
 
         isCorrectInput = true;
 
-        if (!scan_resN || !scan_resK || lengthArrayK <= 0 || lengthArrayN <= 0)
+        if (!scanResultN || !scanResultK || lengthArrayK <= 0 || lengthArrayN <= 0 || lengthArrayK > arraySize || lengthArrayN > arraySize)
         {
-            printf("Incorrect input (\"N\" and \"K\" are positive and less than %d). Try again!\n", arraySize);
+            printf("Incorrect input (\"N\" and \"K\" are positive and less than %d). Try again!\n", arraySize + 1);
             scanf_s("%*[^\n]");
 
             isCorrectInput = false;
         }
-    } while (!scan_resN || !scan_resK);
+    } while (!isCorrectInput);
 
     int arrayN[arraySize] = {0};
     int arrayK[arraySize] = {0};
@@ -145,7 +144,7 @@ int main()
 
     for (int i = 0; i < lengthArrayK; ++i)
     {
-        if (binarySearch(arrayN, 0, lengthArrayN - 1, arrayK[i]))
+        if (binarySearchInSortedArray(arrayN, 0, lengthArrayN - 1, arrayK[i]))
         {
             printf("\"%d\" is present in the array \"N\".\n", arrayK[i]);
         }
