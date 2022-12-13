8f6365eb68861b4a798a30fd3a213fdba6873105
Johanlib <nikgooder@gmail.com>
2022-12-13 18:55:24 +0300

    Add gitignore and add function printArr in 3 task

--- HWs/HW1/task3.c
+++ HWs/HW1/task3.c
@@ -5,7 +5,7 @@
 
 void reverse(int array[], int length)
 {
-    for (int i = 0; i < (length / 2); ++i)
+    for (int i = 0; i < length / 2; ++i)
     {
         int clipboard = 0;
 
@@ -13,7 +13,20 @@ void reverse(int array[], int length)
         array[i] = array[length - (i + 1)];
         array[length - (i + 1)] = clipboard;
     }
+}
 
+void printArray(int array[], int length, int key)
+{
+    printf("[ ");
+    for (int k = 0; k < length; ++k)
+    {
+        printf("%d ", array[k]);
+        if (k == key - 1)
+        {
+            printf("| ");
+        }
+    }
+    printf("]\n");
 }
 
 int main()
@@ -21,7 +34,7 @@ int main()
     int lengthN = 0;
     int lengthM = 0;
     int array[arraySize] = { 0 };
-    int scanf_res = 0;
+    int scanRes = 0;
     bool flagInput = true;
 
     do
@@ -29,48 +42,31 @@ int main()
         flagInput = true;
 
         printf("Enter length \"N\" then length \"M\": ");
-        scanf_res = scanf("%d%d", &lengthN, &lengthM);
+        scanRes = scanf("%d%d", &lengthN, &lengthM);
 
-        if (!scanf_res || lengthM <= 0 || lengthN <= 0 || (lengthN + lengthM > arraySize))
+        if (!scanRes || lengthM <= 0 || lengthN <= 0 || lengthN + lengthM > arraySize)
         {
             printf("Invalid values (length N and length M must be positive and in total no more than %d). Try again!\n", arraySize);
             scanf("%*[^\n]");
             flagInput = false;
         }
-    } while (!scanf_res || !flagInput);
+    } while (!flagInput);
 
     printf("Enter an array, exactly %d element(s):\n", lengthM + lengthN);
     for (int i = 0; i < lengthM + lengthN; ++i)
     {
         scanf("%d", &array[i]);
     }
-   
 
-    printf("\nThe array was like this:\n[ ");
-    for (int k = 0; k < lengthN + lengthM; ++k)
-    {
-        printf("%d ", array[k]);
-        if (k == lengthM - 1)
-        {
-            printf("| ");
-        }
-    }
-    printf("]\n");
+    printf("\nThe array was like this:\n");
+    printArray(array, lengthM + lengthN, lengthM);
 
     reverse(array, lengthM);
     reverse(array, lengthM + lengthN);
     reverse(array, lengthN);
-  
-    printf("\nThe array become like this:\n[ ");
-    for (int k = 0; k < lengthN + lengthM; ++k)
-    {
-        printf("%d ", array[k]);
-        if (k == lengthN - 1)
-        {
-            printf("| ");
-        }
-    }
-    printf("]\n");
+
+    printf("\nThe array become like this:\n");
+    printArray(array, lengthM + lengthN, lengthN);
 
     return 0;
 }
