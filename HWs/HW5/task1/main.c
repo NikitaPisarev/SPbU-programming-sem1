f3fe54e57ab6011d59a0f14e414084a2ba7ed734
Johanlib <nikgooder@gmail.com>
2022-12-16 01:41:51 +0300

    Add function postfixNotationCalculation

--- HWs/HW5/task1/main.c
+++ HWs/HW5/task1/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "../Stack/stack.h"
+
+void printError()
+{
+    printf("// Error codes:\n");
+    printf("-1: Stack not created\n");
+    printf("-2: Stack head is missing(or memory allocation error for head)\n");
+    printf("-3: Memory allocation error\n");
+}
 
 int operations(Stack *stack, unsigned char sign)
 {
@@ -68,88 +77,26 @@ int operations(Stack *stack, unsigned char sign)
     return 0;
 }
 
-bool testOperations()
+int postfixNotationCalculation(char string[], int lengthString, int *result)
 {
-    char testEntry[] = "9 6 - 1 2 + *";
-
     Stack *stack = createStack();
     if (stack == NULL)
     {
         printf("Stack creation error.\n");
-        return false;
+        return -1;
     }
 
-    for (int i = 0; i < 13; ++i)
-    {
-        unsigned char currentСharacter = testEntry[i];
-
-        if (currentСharacter == ' ')
-        {
-            continue;
-        }
-
-        if (currentСharacter == '+' || currentСharacter == '-' || currentСharacter == '*' || currentСharacter == '/')
-        {
-            if (operations(stack, currentСharacter) != 0)
-            {
-                printf("Error in operations function.\n");
-                freeStack(stack);
-                return false;
-            }
-
-            continue;
-        }
-
-        if (push(stack, (int)currentСharacter - (int)'0') != 0)
-        {
-            printf("Error in push function.\n");
-            freeStack(stack);
-            return false;
-        }
-    }
-
-    int resultTest = 0;
-    if (pop(stack, &resultTest) != 0)
-    {
-        printf("Error in pop function.\n");
-        freeStack(stack);
-        return false;
-    }
-
-    freeStack(stack);
-    return resultTest == 9;
-}
-
-int main()
-{
-    if (!testOperations())
-    {
-        printf("Tests failed!\n");
-        return 0;
-    }
-
-    printf("Enter the expression in reverse Polish notation:\n");
-
-    Stack *stack = createStack();
-    if (stack == NULL)
-    {
-        printf("Stack creation error.\n");
-        return 0;
-    }
-
-    unsigned char currentСharacter = 0;
     int errorCode = 0;
-
-    while ((currentСharacter = getc(stdin)) != '\n')
+    for (int i = 0; i < lengthString; ++i)
     {
-        if (currentСharacter == ' ')
+        if (string[i] == ' ')
         {
             continue;
         }
 
-        if (currentСharacter == '+' || currentСharacter == '-' || currentСharacter == '*' || currentСharacter == '/')
+        if (string[i] == '+' || string[i] == '-' || string[i] == '*' || string[i] == '/')
         {
-            if ((errorCode = operations(stack, currentСharacter)) != 0)
+            if ((errorCode = operations(stack, string[i])) != 0)
             {
                 if (errorCode == -4)
                 {
@@ -158,45 +105,96 @@ int main()
                 else
                 {
                     printf("The operations function failed with error %d.\n", errorCode);
+                    printError();
                 }
                 freeStack(stack);
-                return 0;
+                return -1;
             }
 
             continue;
         }
 
-        if (currentСharacter < '0' || currentСharacter > '9')
+        if (string[i] < '0' || string[i] > '9')
         {
             printf("Reading error, enter only \"+, -, *, /\" and digits 0-9.\n");
             freeStack(stack);
-            return 0;
+            return -1;
         }
 
-        if ((errorCode = push(stack, (int)(currentСharacter - '0'))) != 0)
+        if ((errorCode = push(stack, (int)(string[i] - '0'))) != 0)
         {
             printf("The push function failed with error %d.\n", errorCode);
+            printError();
             freeStack(stack);
-            return 0;
+            return -1;
         }
     }
 
-    int result = 0;
-    if ((errorCode = pop(stack, &result)) != 0)
+    if ((errorCode = pop(stack, result)) != 0)
     {
         printf("The pop function failed with error %d.\n", errorCode);
+        printError();
         freeStack(stack);
-        return 0;
+        return -1;
     }
 
-    if (isEmpty(stack))
+    if (!isEmpty(stack))
     {
-        printf("");
+        printf("Incorrect input.\n");
         freeStack(stack);
         return -1;
     }
 
     freeStack(stack);
+    return 0;
+}
+
+bool testOperations()
+{
+    char testEntry[] = "9 6 - 1 2 + *";
+    int resultTest = 0;
+    int errorCode = postfixNotationCalculation(testEntry, 13, &resultTest);
+    if (errorCode != 0)
+    {
+        return false;
+    }
+    return resultTest == 9;
+}
+
+int main()
+{
+    if (!testOperations())
+    {
+        printf("Tests failed!\n");
+        return -1;
+    }
+
+    int lengthString = 0;
+    printf("Enter the size of the Polish record: ");
+    scanf("%d", &lengthString);
+    if (lengthString <= 0)
+    {
+        printf("Incorrect record size.\n");
+        return -1;
+    }
+    char *string = calloc(lengthString + 1, sizeof(char));
+    if (string == NULL)
+    {
+        printf("Memory allocation error.\n");
+        return -1;
+    }
+    printf("Enter the expression in reverse Polish notation:\n");
+    getchar();
+    fgets(string, lengthString + 1, stdin);
+
+    int result = 0;
+    int errorCode = postfixNotationCalculation(string, lengthString, &result);
+    if (errorCode != 0)
+    {
+        return -1;
+    }
+
     printf("Answer: %d", result);
+    free(string);
     return 0;
 }
